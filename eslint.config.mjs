be7bd3f57b956d64a145cd27eecc2cfe01e56adb
uvarovag <aleanches@gmail.com>
2025-01-18 02:19:47 +0300

    feat: move webpack config in package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
 import reactConfig from '@uvarovag/eslint-config-ts-react'
+import featureSlicedConfig from '@uvarovag/eslint-config-feature-sliced-flat'
 
-export default [...reactConfig]
+export default [...featureSlicedConfig, ...reactConfig]
